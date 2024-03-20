b7b96dcc6629cdfe7bd1cb091983ee4d9b0cbf5a
Atalay Yaman <atalayyaman770@gmail.com>
2024-03-20 14:56:01 +0300

    Lab 3; Assigment 3 comment lines added.

--- WorkSpace/Lab3/Assigment3.py
+++ WorkSpace/Lab3/Assigment3.py
@@ -1,7 +1,19 @@
+# Assigment 3
+
+"""
+Üç liste oluşturun: sırasıyla öğrencilerin adlarını, öğrenci kimliklerini ve notlarını içeren names, student_ids ve
+grades. Bu listeleri aynı anda yinelemek için zip() işlevini kullanın ve her öğrencinin bilgilerini şu biçimde yazdırın:
+ "İsim: <name>, Öğrenci Kimliği: <student_id>, Grade: <not>" biçiminde yazdırın.
+"""
+
+# Öğrencilere ait bilgiler tanımlandı.
 names = ("Ali","Veli","Ahmet","Ayşe")
 ids = (1,2,3,4)
 grades = (85,95,90,70)
 
+# Bütün öğrenci bilgileri önce ziplenip sonrasında tuple içerisine alındı.
 holdStudent = tuple(zip(names, ids, grades))
+
+# Ziplenmiş tuple'ı(holdStudent) for döngüsüyle yazdırıldı.
 for name, id, grade in holdStudent:
     print(f"İsim: {name} Öğrenci no: {id} Not: {grade}")
